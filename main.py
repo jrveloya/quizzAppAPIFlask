be8ca17951ef4819d8c2aa910cdf5393968f5ad1
Robert Veloya <80503404+jrveloya@users.noreply.github.com>
2025-03-23 19:41:59 -0700

    Update main.py
    Added port settings for deployment on AWS EC2

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         insert_test_data()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
